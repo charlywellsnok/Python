77748c07c2ca1d6829f2770ad4aea44ae93487d0
charlywellsnok <wellingtoncharlottek@gmail.com>
2025-08-18 08:36:20 +0000

    Added new files

---
+++ programiz_prac/2sum_bruteforce.py
@@ -0,0 +1,45 @@
+1. Two Sum
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                   return [i, j]
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+# Define input
+nums = [2, 7, 11, 15]
+target = 9
+
+# Create an instance of Solution
+sol = Solution()
+
+# Call the method using the object
+total = sol.twoSum(nums, target)
+
+# Print result
+print(total)
+
